b061b72f2055bbb98f2136fb74e66488da7f5507
YeJunLim <dpwns523@gmail.com>
2022-07-01 15:31:06 +0900

    boot_setting

---
+++ src/main/java/com/example/setting_springboot/SettingSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.setting_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettingSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SettingSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/setting_springboot/SettingSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.setting_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettingSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
