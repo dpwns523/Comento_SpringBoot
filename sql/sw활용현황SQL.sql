80b6215e97116d4752eaeeecdb8d0fc1600001d7
dpwns523 <84260096+dpwns523@users.noreply.github.com>
2022-07-09 13:51:19 +0900

    Update sw활용현황SQL.sql

--- sql/sw활용현황SQL.sql
+++ sql/sw활용현황SQL.sql
@@ -1,25 +1,28 @@
 -- 년월별 접속자 수
-select substring(createDate, 1, 4) as 'yearMonth', count(case when requestcode='L' then 1 end) as totCnt
-from statistc.requestInfo
-group by substring(createDate, 1, 4);
+select substring(`createDate`, 1, 4) as `date`, count(case when `requestcode`=? then 1 end) as `totCnt`
+from statistc.requestinfo
+where substring(`createDate`, 1, 2)= ? and `requestcode` = ?
+group by `date` order by `date` DESC;
 
 -- 일자별 접속자 수
-select substring(createDate, 1, 6) as date, count(case when requestcode='L' then 1 end) as totCnt
+select substring(`createDate`, 1, 6) as `date`, count(case when `requestcode`=? then 1 end) as `totCnt`
 from statistc.requestinfo
-group by createDate; 
+where substring(`createDate`, 1, 4)= ? and `requestcode` = ?
+group by `date`  order by `date` DESC;
 
 -- 평균 하루 로그인 수
-select substring(createDate, 1, 6) as date, count(case when requestcode='L' then 1 end) / count(distinct createDate) as meanCnt
+select count(case when `requestcode` = ? then 1 end) / count(distinct substring(`createDate`, 1, 6)) as `meanCnt`
 from statistc.requestinfo
-group by createDate order by meanCnt DESC;
+where `requestcode` = ?;
 
 -- 휴일을 제외한 로그인 수 (현재 버전은 휴일 포함)
 select count(case when requestcode='L' then 1 end) as totCnt
 from statistc.requestinfo;
 
 -- 부서별 년월별 로그인 수
-select u.HR_ORGAN as 'department', substring(ri.createDate, 1, 4) as 'yearMonth', count(case when requestcode='L' then 1 end) as totCnt
-from statistc.requestInfo as ri
+select u.HR_ORGAN as `dep`, substring(ri.createDate, 1, 4) as `date`, count(case when ri.requestcode= ? then 1 end) as `totCnt`
+from statistc.requestinfo as ri
 join statistc.user as u on u.userID = ri.userID
+where substring(ri.createDate, 1, 2)= ? and ri.requestcode = ?
 group by u.HR_ORGAN, substring(ri.createDate, 1, 4)
 order by u.HR_ORGAN, substring(ri.createDate, 1, 4) DESC;
